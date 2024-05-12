c684f37ea76e0cc3489e75f3a660eec26d011c6f
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-05-12 23:53:24 +0530

    first commit

---
+++ src/api/websocket/controllers/websocket.js
@@ -0,0 +1,32 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server( { port: 1440} );
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    // Echo the received message back to the client
+    const stringMessage = message.toString();
+    console.log('Received message:', message);
+    ws.send(stringMessage);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+
+  ws.on('error', (error) => {
+    console.error('WebSocket error:', error);
+  });
+});
+
+module.exports = {
+  connect: async (ctx) => {
+    ctx.websocket.server.on('upgrade', (request, socket, head) => {
+      wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request);
+      });
+    });
+  }
+};
